c9ddfd14e5bc48b19225d1e84052287965101fd5
priyankap <priyanka123360@gmail.com>
2021-10-31 14:54:06 +0530

    Added : Equal length logic

--- src/com/bridgelabz/demo/Line_Comparison.java
+++ src/com/bridgelabz/demo/Line_Comparison.java
@@ -5,20 +5,30 @@ import java.util.Scanner;
 public class Line_Comparison {
 	public static void main(String[] args) {
 
-		int x1, x2, y1, y2;
+		int x1, x2, y1, y2, p1, p2, q1, q2;
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("enter x1,x2,y1,y2 point");
+		System.out.println("enter x1, x2, y1, y2, p1, p2, q1, q2 point");
 
 		x1 = sc.nextInt();
 		x2 = sc.nextInt();
 		y1 = sc.nextInt();
 		y2 = sc.nextInt();
+		p1 = sc.nextInt();
+		p2 = sc.nextInt();
+		q1 = sc.nextInt();
+		q2 = sc.nextInt();
 
-		double length = (double) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		double length1 = (double) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		double length2 = (double) Math.sqrt((p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1));
+		System.out.println("length of line1 = " + length1);
+		System.out.println("length of line2 = " + length2);
+		if (length1 == length2) {
+			System.out.println(" Both lines are equal in length");
 
-		System.out.println("length of line = " + length);
-
+		} else {
+			System.out.println(" Both lines have different length");
+		}
 	}
 }
