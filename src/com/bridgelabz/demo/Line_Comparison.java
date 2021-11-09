fe23b88622fed6dd34bc3b7854de31f9c15a586b
priyankap <priyanka123360@gmail.com>
2021-11-09 18:18:14 +0530

    Added : Compare two Lines using CompareTo Method.

--- src/com/bridgelabz/demo/Line_Comparison.java
+++ src/com/bridgelabz/demo/Line_Comparison.java
@@ -3,42 +3,50 @@ package com.bridgelabz.demo;
 import java.util.Scanner;
 
 public class Line_Comparison {
-	public static void main(String[] args) {
+	int x1, x2, y1, y2;
 
-		int x1, x2, y1, y2, p1, p2, q1, q2;
+	public double Length() {
 
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("enter x1, x2, y1, y2, p1, p2, q1, q2 point");
-
+		System.out.println("enter x1, x2, y1, y2 point");
 		x1 = sc.nextInt();
 		x2 = sc.nextInt();
 		y1 = sc.nextInt();
 		y2 = sc.nextInt();
-		p1 = sc.nextInt();
-		p2 = sc.nextInt();
-		q1 = sc.nextInt();
-		q2 = sc.nextInt();
 
-		double length1 = (double) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-		double length2 = (double) Math.sqrt((p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1));
-		System.out.println("length of line1 = " + length1);
-		System.out.println("length of line2 = " + length2);
+		double length = (double) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println("length of line = " + length);
+		return length;
+	}
+
+	public static void compareTo(double length1, double length2) {
+
 		if (length1 == length2) {
 			System.out.println(" Both lines are equal in length");
 
 		} else {
 			if (length1 < length2) {
 				System.out.println(" Length of line 2 is greater than line 1 ");
+
 			} else {
 				if (length1 > length2) {
 					System.out.println(" Length of line 1 is greater than line 2 ");
+
 				} else {
 					System.out.println(" Both lines have different Length ");
 				}
 			}
-
 		}
 
 	}
+
+	public static void main(String[] args) {
+		double length1, length2;
+		Line_Comparison length = new Line_Comparison();
+		length1 = length.Length();
+		length2 = length.Length();
+		compareTo(length1, length2);
+	}
+
 }
